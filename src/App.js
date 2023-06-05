dc0277ed979d0dea784f7766c312accd6b5ca2a2
aridguy <rotimiariyo@gmail.com>
2023-06-05 22:23:38 +0100

    innitial commits for all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import axios, { Axios } from 'axios';
+import axios from 'axios';
 import { useState } from 'react';
 import Lol from "../src/loll.PNG"
 
